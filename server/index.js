55d06b15dad318331bfdd092e83233d18d50d808
Vikum Viraj <Mvikumviraj@gmail.com>
2023-07-27 09:47:59 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+import cors from 'cors'
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
+app.listen(PORT,() => {
+    console.log(`server is runing on ${PORT}`)
+})
